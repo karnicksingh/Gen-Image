e85a868dcdf244522e4e5bfd7f361aed606ff4b4
karnick singh <karnicksingh61@gmail.com>
2026-04-28 02:36:35 +0530

    added mongo atals

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB =async()=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/AI-Database");
+        await mongoose.connect("mongodb+srv://admin:0004@genimage.1btltct.mongodb.net/");
         console.log("DB connected");
 
     }catch(error){
