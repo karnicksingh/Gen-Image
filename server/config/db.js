2ed03f5d675cbd8cb23d7a5e688d6981cf76be7f
karnick singh <karnicksingh61@gmail.com>
2026-06-02 13:05:12 +0530

    db Updated

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 
 const connectDB =async()=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/AI-Database");
+        // await mongoose.connect("mongodb://127.0.0.1:27017/AI-Database");
+        // console.log("DB connected");
+        await mongoose.connect(`mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.vbujf0j.mongodb.net/`);
         console.log("DB connected");
 
     }catch(error){
